11be573db3618988264bcf723e176039e2dc3e2b
isham2024 <132916476+isham2024@users.noreply.github.com>
2023-05-18 23:20:15 +0530

    Update env.d.ts

--- env.d.ts
+++ env.d.ts
@@ -4,16 +4,16 @@ declare global {
             // Server-side environment variables should be named using uppercase
             // letters only, while camelCase naming convention should be used
             // for variables shared by both the client-side and server-side.
-            appName: string;
-            appLogo: string;
+            appName: "ARCH";
+            appLogo: "https://www.flaticon.com/free-icon/accenture_6128893";
             appThemeColor: string;
-            appSummary: string;
-            exampleInput: string;
+            appSummary: "Stay up-to-date on your firm-specific regulations";
+            exampleInput: "What is Basel III?";
 
-            OPENAI_API_KEY: string;
+            OPENAI_API_KEY: "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";
             OPENAI_API_BASE_URL: string;
-            SYSTEM_MESSAGE: string;
-            MESSAGE_TEMPLATE: string;
+            SYSTEM_MESSAGE: "Learn about regulations specific to your firm";
+            MESSAGE_TEMPLATE: "Enter your regulatory requirement";
         }
     }
 }
